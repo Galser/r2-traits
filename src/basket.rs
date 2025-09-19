4a2246010f5c77f5aff93dc606410b818af92cbf
galser <guseletov@gmail.com>
2025-09-19 15:26:28 +0200

    Before Stack

--- src/basket.rs
+++ src/basket.rs
@@ -1,18 +1,18 @@
-pub struct Basket {
-    item: Option<String>,
+pub struct Basket<T> {
+    item: Option<T>,
 }
 
-impl Basket {
+impl<T> Basket<T>{
     
-    pub fn new(item: String) -> Self{
+    pub fn new(item: T) -> Self{
         Basket { item: Some(item) }
     }
 
-    pub fn get(&mut self) -> Option<String> {
+    pub fn get(&mut self) -> Option<T> {
         self.item.take()
     }
 
-    pub fn put(&mut self, item: String) {
+    pub fn put(&mut self, item: T) {
         self.item = Some(item)
     }
 
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,8 @@ use basket::Basket;
 
 fn main() {
     let b1 = Basket::new(String::from("Hello there"));
-    println!("Hello, world!");
+    
+    let b2 = Basket::new(10);
+
+    let b3 = Basket::new(true);
 }
