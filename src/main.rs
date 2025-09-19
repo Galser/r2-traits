7185e31bc3f1175dcacc980979e4405066412edf
galser <guseletov@gmail.com>
2025-09-19 16:09:32 +0200

    Wrap-up

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,19 @@ use basket::Basket;
 use stack::Stack;
 use container::Container;
 
+fn add_string<T:Container<String>>(c: &mut T, s: String) {
+    c.put(s)
+}
 
 fn main() {
-    let b1 = Basket::new(String::from("Hello there"));
+    let mut b1 = Basket::new(String::from("Hello there"));
     let b2 = Basket::new(10);
     let b3 = Basket::new(true);
 
-    let s1 = Stack::new(vec![String::from("Hi!")]);
+    let mut s1 = Stack::new(vec![String::from("Hi!")]);
     let s2 = Stack::new(vec![1,2,3]);
+
+    add_string(&mut b1, String::from("Hallo!"));
+
+    add_string(&mut s1, String::from("Hallo in stack!"));
 }
