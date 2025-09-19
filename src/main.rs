3f16769eb8093de9a9ba68ec4809d2eb22da7dbc
galser <guseletov@gmail.com>
2025-09-19 15:32:19 +0200

    Stack init

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 mod basket;
+mod stack;
+
 use basket::Basket;
+use stack::Stack;
+
 
 fn main() {
     let b1 = Basket::new(String::from("Hello there"));
-    
     let b2 = Basket::new(10);
-
     let b3 = Basket::new(true);
+
+    let s1 = Stack::new(vec![String::from("Hi!")]);
+
 }
